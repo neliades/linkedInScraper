395dc3ae18ad4e588a4c924e10a1288983f5b5bb
neliades <nicholas.a.eliades@gmail.com>
2019-02-07 14:51:11 -0500

    Code cleanup

--- db/addCompany.js
+++ db/addCompany.js
@@ -1,184 +1,20 @@
-console.log('\n\n\n\n\n***new test***\n\n\n\n\n')
+// console.log('\n\n\n\n\n***new test***\n\n\n\n\n')
 
-const mysql = require('mysql');
-const mysqlConfig = require('./config.js');
-const testData = require('./testData.js')
-const connection = mysql.createConnection(mysqlConfig);
-connection.connect( (err) => {
-    if (err) throw err;
-    console.log("mySQL connected successfully");
-})
+const testData = require('./testData.js');
 
+const {checkIfExists} = require('./helpers.js');
+const {insertQuery} = require('./helpers.js')
+const {checkIfExistsInJunction} = require('./helpers.js')
+const {insertQueryIfNotExists} = require('./helpers.js')
+const {updateOneFieldDB} = require('./helpers.js')
+const {findLastId} = require('./helpers.js')
 
-
-const checkIfExists = (companyInfo, tableName, field, value, cbExists, cbNotExists) => {
-
-    let sql = `SELECT * FROM ${tableName} WHERE ${field} = '${value}'`
-    connection.query(sql, null, (err, results) => {
-        if(err) {
-            console.log(err)
-            throw new Error("Error in checkIfExists", err)
-          } else {
-            if (results.length < 1) {
-                //add
-                console.log('!!!!!!!!!!!!!!!!!!!!!')
-                console.log('does not exist')
-                console.log(sql)
-                console.log()
-                if (cbNotExists) {
-                    cbNotExists(companyInfo, results);
-                }
-            } else {
-                console.log('!!!!!!!!!!!!!!!!!!!!!')
-                console.log('already exists')
-                console.log(sql)
-                console.log()
-                console.log(results[0])
-                if (cbExists) {
-                    cbExists(companyInfo, results);
-                }
-            }
-        }
-    })
-    
-}
+const connection = require('./connectToDb.js');
 
 
-const selectFromDB = (companyInfo, cb) => {
-    let sql = `SELECT * FROM ${tableName} WHERE name = '${companyInfo.overview.name}'`;
-    connection.query(sql, null, (err, results) => {
-        if(err) {
-            console.log(err)
-            throw new Error("Error in selectFromDB", err)
-          } else {
-            console.log(results);
-          }
-    })
-}
 
-const selectLastTable = (tableName, fieldLookup, valueLookup, field, isJunction, cb) => {
-    let sql = `SELECT LAST_INSERT_ID()`;
-    connection.query(sql, null, (err, results) => {
-        if(err) {
-            console.log(err)
-            throw new Error("Error in selectLastTable", err)
-        } else {
-            let lastID = results[0]['LAST_INSERT_ID()'];
-            //table, company name, foreign field, last id
-            if (isJunction) {
-                junctionUpdateAdd();
-            } else {
-                updateOneFieldDB(tableName, fieldLookup, valueLookup, field, lastID, cb);
-            }
-            // return (results[0]['LAST_INSERT_ID()']);
-          }
-    })
-}
-const insertQuery = (tableName, fields, values, cb) => {
-    let sql = `INSERT INTO ${tableName} (${fields})
-    VALUES (?)`;
-    console.log(sql);
-    console.log()
-    connection.query(sql, [values], (err, results) => {
-        if(err) {
-            console.log(err)
-            throw new Error("Error in insertQuery", err)
-          } else {
-            console.log(results);
-            console.log('completed: ', tableName)
-            if (cb) cb();
-          }
-    })
-}
-
-const checkIfExistsInJunction = (tableName, fields, values, cbExists, cbNotExists) => {
-    console.log('checking junction for first two fields')
-    let sql = `SELECT * FROM ${tableName} WHERE ${fields[0]} = '${values[0]}' AND ${fields[1]} = '${values[1]}'`;
-    connection.query(sql, null, (err, results) => {
-        if(err) {
-            console.log(err)
-            throw new Error("Error in checkIfExistsInJunction", err)
-          } else {
-            if (results.length < 1) {
-                //add
-                console.log('this combination of foreign ids DOES NOT exist, adding now...')
-                if (cbNotExists) {
-                    console.log('invoking the callback for does not exist')
-                    cbNotExists(results);
-                }
-            } else {
-                console.log('this combination of foreign ids ALREADY exists')
-                console.log(results[0])
-                if (cbExists) {
-                    console.log('invoking the callback for exists')
-                    cbExists(results);
-                }
-            }
-        }
-    })
-}
-
-const insertQueryIfNotExists = (tableName, fields, values, cb) => {
-    let field = fields.split(',')[0]; //name
-    let value;
-    typeof values === "object" ? value = values[0] : value = values; //internet
-    console.log('XXXXXXXXXXXXXXXXXXXXXXX')
-    console.log(tableName, ': ', value)
-    checkIfExists(null, tableName, field, value, (companyInfo, results) => {
-        let id = results[0].id;
-        //if exists
-        //if companyName or industryName or other unique fields, don't update
-        //if a junction, check if the second Id matches the one you're going to add
-            //if exists, don't add
-            //if does not exist, add
-        if (tableName.split('_').length > 1) {
-            fieldsArr = fields.split(',');
-            console.log('already exists but it is a junction, so checking if both ids exist')
-            checkIfExistsInJunction(tableName, fieldsArr, values, cb, () => {
-              insertQuery (tableName, fields, values, cb) 
-            });
-        } else {
-            cb(id);
-        }
-    }, (companyInfo, results) => {
-        //if doesn't exist, add
-        console.log('doesnt exist, performing insertQuery')
-        insertQuery (tableName, fields, values, cb) 
-    })
-}
-
-const updateOneFieldDB = (tableName, fieldLookup, valueLookup, field, value, cb) => {
-    // UPDATE table_name
-    // SET column1 = value1, column2 = value2, ...
-    let sql = `UPDATE ${tableName}
-    SET ${field} = ${value}
-    WHERE ${fieldLookup} = '${valueLookup}'`;
-
-    connection.query(sql, null, (err, results) => {
-        if(err) {
-            console.log(err)
-            throw new Error("Error in updateOneFieldDB", err)
-          } else {
-            console.log(results);
-            if (cb) cb();
-        }
-    })
-}
-
-const findLastId = (cb) => {
-    let sql = `SELECT LAST_INSERT_ID()`;
-    connection.query(sql, null, (err, results) => {
-        if(err) {
-            console.log(err)
-            throw new Error("Error in selectLastTable", err)
-        } else {
-            let lastID = results[0]['LAST_INSERT_ID()'];
-            cb(lastID);
-          }
-    })
-}
 const insertToDB = async function (companyInfo, results, specialQuery, cb) {
-    console.log('insertToDB invoked')
+    // console.log('insertToDB invoked')
     let queryInfo = {};
     if (companyInfo) {
         queryInfo = {
@@ -269,13 +105,13 @@ const insertToDB = async function (companyInfo, results, specialQuery, cb) {
         queryInfo = specialQuery;
     }
     let insertForeignAndWait = (propertyName) => {
-            console.log('starting foreign and junctions: ', propertyName);
+            // console.log('starting foreign and junctions: ', propertyName);
             return new Promise (resolve => {
                 let values = [];
                 let foreignIdList = queryInfo[propertyName].foreignIdList;
-                console.log('foreignIdList for , ', propertyName, ': ', foreignIdList)
+                // console.log('foreignIdList for , ', propertyName, ': ', foreignIdList)
                 if (foreignIdList !== undefined) {
-                    console.log('foreignIdList found at table: ', propertyName)
+                    // console.log('foreignIdList found at table: ', propertyName)
                     for (let i =0; i < foreignIdList.length; i++) {
                         if (foreignIdList[i] === 'self') {
                             values.push(queryInfo[propertyName].id);
@@ -285,19 +121,19 @@ const insertToDB = async function (companyInfo, results, specialQuery, cb) {
                             values.push(queryInfo[foreignIdList[i]].id);
                         }
                     }
-                    console.log('idList for ', queryInfo[propertyName].targetTable, ': ', values);
+                    // console.log('idList for ', queryInfo[propertyName].targetTable, ': ', values);
                     let targetTable = queryInfo[propertyName].targetTable;
                     if (targetTable === 'companies') {
                         //const updateOneFieldDB = (propertyName, fieldLookup, valueLookup, field, value, cb) => {
                         updateOneFieldDB(targetTable, 'id', queryInfo[targetTable].id, queryInfo[propertyName].foreignFields, values[0], () => {
-                            console.log('inserted id_industries into companies');
+                            // console.log('inserted id_industries into companies');
                             resolve();
                         })
                     } else {
                         //find if exists
                         insertQueryIfNotExists(targetTable, queryInfo[propertyName].foreignFields, values, () => {
                             //if exists or if just added, save the id to the object
-                            console.log('successful entry for ', queryInfo[propertyName].targetTable, ': ', values);
+                            // console.log('successful entry for ', queryInfo[propertyName].targetTable, ': ', values);
                             resolve();
                         });
                     }
@@ -307,7 +143,7 @@ const insertToDB = async function (companyInfo, results, specialQuery, cb) {
             });
     }
     let insertAndWait = (propertyName) => {
-        console.log('starting initial inserts: ', propertyName);
+        // console.log('starting initial inserts: ', propertyName);
         return new Promise (resolve => {
             //find if exists
             let tableName = propertyName.split('_')[0];
@@ -333,38 +169,24 @@ const insertToDB = async function (companyInfo, results, specialQuery, cb) {
     for (let propertyName in queryInfo) {
         await insertForeignAndWait(propertyName);
     }
-    console.log('queryInfo: ', queryInfo)
+    await console.log('inserted company data');
+    await connection.end();
+    // console.log('queryInfo: ', queryInfo)
 }
 
 
 
-const selectAllAboutCompany = () => {
-    let sql = `SELECT companies.name, companies.website, companies.size, industries.name, companies.description, companies.rating, companies.recommended, companies.ceoApproval, companies.locatedInNyc, companies.positive, companies.neutral, companies.negative, companies.difficulty
-    FROM companies
-    INNER JOIN industries
-    ON companies.id_industries=industries.id
-    WHERE companies.name = 'Google'
-    ;`;
-
-    connection.query(sql, null, (err, results) => {
-        if(err) {
-            console.log(err)
-            throw new Error("Error in selectAllAboutCompany", err)
-          } else {
-            console.log(results);
-          }
-    })
-}
 
 
-const addToDBIfNotExists = (companyInfo, cb) => {
+
+const addCompanyIfNotExists = (companyInfo, cb) => {
     checkIfExists(companyInfo, 'companies', 'name', companyInfo.overview.name, null, insertToDB);
 }
 
-addToDBIfNotExists(testData);
+// addCompanyIfNotExists(testData);
 
 
 
 
-module.exports.addToDBIfNotExists = addToDBIfNotExists;
+module.exports.addCompanyIfNotExists = addCompanyIfNotExists;
 
